0a7f20cbd068095862d7cc81b82d711a4ca5fd99
alchogh <alchogh01@gmail.com>
2024-05-03 01:03:23 +0900

    add : put, delete, 예외처리

--- demo-api/youtuber-demo.js
+++ demo-api/youtuber-demo.js
@@ -17,13 +17,13 @@ let youtuber2 = {
 };
 
 let db = new Map();
-let id = 1
+let id = 1;
 db.set(id++, youtuber1);
 db.set(id++, youtuber2);
 
 //REST API 설계
-app.get('/youtubers', (_req, res)=>{
-  res.json([...db.values()])
+app.get("/youtubers", (_req, res) => {
+  res.json([...db.values()]);
   //방법1
   // const youtubersArray = Array.from(db.values());
   //res.json(youtubersArray);
@@ -34,10 +34,9 @@ app.get('/youtubers', (_req, res)=>{
   //   youtubersObject[key] = value;
   // });
   // res.json(youtubersObject);
-})
+});
 
-
-app.get("/youtuber/:id", function (req, res) {
+app.get("/youtubers/:id", function (req, res) {
   let { id } = req.params;
   id = parseInt(id);
   const youtube = db.get(id);
@@ -49,15 +48,60 @@ app.get("/youtuber/:id", function (req, res) {
   }
 });
 
-app.use(express.json()) //http 외 모듈인 '미들웨어' : json 설정
-app.post('/youtuber', (req,res)=>{
+app.use(express.json()); //http 외 모듈인 '미들웨어' : json 설정
+app.post("/youtubers", (req, res) => {
   //body에 숨겨져서 들어온 데이터를 화면에 뿌려줘볼가?
-
-  console.log(req.body)
-
-  db.set(id++,req.body)
-
+  db.set(id++, req.body);
   res.json({
-    message: `${req.body.channelTitle}님, 유투버 생활을 응원합니다`
-  })
-})
+    message: `${req.body.channelTitle}님, 유투버 생활을 응원합니다`,
+  });
+});
+
+app.delete("/youtubers/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  let youtuber = db.get(id);
+  if (youtuber == undefined) {
+    res.json({
+      message: `요청하신${id}번은 없는 유투버입니다.`,
+    });
+  } else {
+    const channelTitle = youtuber.channelTitle;
+
+    db.delete(id);
+    res.json({
+      message: `${channelTitle}님 채널이 삭제되었습니다.`,
+    });
+  }
+});
+
+app.delete("/youtubers", (_req, res) => {
+  let msg = "";
+  if (db.size === 0) {
+    msg = "삭제할 유투버가 없습니다.";
+  } else {
+    db.clear();
+    msg = "전체 삭제가 되었습니다.";
+  }
+  res.json({ message: msg });
+});
+
+app.put("/youtubers/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  let youtuber = db.get(id);
+  if (youtuber == undefined) {
+    res.json({
+      message: `요청하신${id}번은 없는 유투버입니다.`,
+    });
+  } else {
+    let newTitle = req.body.channelTitle;
+    youtuber.channelTitle = newTitle;
+    db.set(id, youtuber);
+  }
+  res.json({
+    message: "수정됨",
+  });
+});
